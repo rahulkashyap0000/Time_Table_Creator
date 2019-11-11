031924e8efe6bab31cf5daf2602bdeca6691c951
Rahul Kashyap <rahul.kashyap.17001@iitgoa.ac.in>
2019-11-11 11:02:53 +0530

    New Update

---
+++ time_table.py
@@ -0,0 +1,9 @@
+import json
+
+json_file = open('Time_Table.json')
+fi = json.load(json_file)
+room_types = "Room Types"
+insti_time = "Institute time"
+classrooms = "Classrooms"
+courses = "Courses"
+
